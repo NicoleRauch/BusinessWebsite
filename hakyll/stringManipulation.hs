5538a15f2bc681cdea67024e055793829264c510
Nicole Rauch <nicole.m@gmx.de>
2016-07-31 17:04:23 +0200

    We need to propagate the cutoff to keep track of the overall index into the string

--- hakyll/stringManipulation.hs
+++ hakyll/stringManipulation.hs
@@ -4,25 +4,25 @@ import           Data.List (elemIndex, isPrefixOf)
 
 
 getExcerpt :: String -> String
-getExcerpt x = untilCount x $ substringPos x "tests"
+getExcerpt x = untilCount x $ substringPos x "tests" 0
 
 untilCount :: String -> Maybe Int -> String
 untilCount _ Nothing = ""
 untilCount string (Just i) = take i string
 
-substringPos :: String -> String -> Maybe Int
-substringPos _ [] = Nothing
-substringPos [] _ = Nothing
-substringPos string (c:cs) = bla (elemIndex c string) string c cs
+substringPos :: String -> String -> Int -> Maybe Int
+substringPos _ [] _ = Nothing
+substringPos [] _ _ = Nothing
+substringPos string (c:cs) cutoff = bla (elemIndex c string) string c cs cutoff
 
-bla :: Maybe Int -> String -> Char -> String -> Maybe Int
-bla Nothing _ _ _ = Nothing
-bla (Just i) string firstChar remainingChars = foo (drop (i+1) string) firstChar remainingChars i
+bla :: Maybe Int -> String -> Char -> String -> Int -> Maybe Int
+bla Nothing _ _ _ _ = Nothing
+bla (Just i) string firstChar remainingChars cutoff = foo (drop (i+1) string) firstChar remainingChars (i+cutoff)
 
 foo :: String -> Char -> String -> Int -> Maybe Int
 foo remainingString firstChar remainingChars currentIndex
     | isPrefixOf remainingChars remainingString = Just currentIndex
-    | otherwise = substringPos remainingString (firstChar:remainingChars)
+    | otherwise = substringPos remainingString (firstChar:remainingChars) currentIndex
 
 -- drop count list
 -- elemIndex elem list
--- hakyll/stringManipulationTest.hs
+++ hakyll/stringManipulationTest.hs
@@ -9,41 +9,59 @@ main = hspec $ do
     it "returns the current index if the remaining string starts with the remaining chars" $ do
        foo "abcde" 'x' "abc" 7 `shouldBe` (Just 7)
 
+    it "adds the index into the remaining string if it contains the remaining chars" $ do
+       foo "abcde" 'c' "d" 7 `shouldBe` (Just 10)
+
+    it "returns Nothing if the remaining string does not contain remaining chars" $ do
+       foo "abcde" 'x' "abc" 7 `shouldBe` (Just 7)
+
   describe "bla" $ do
     it "returns nothing when there is no current index" $ do
-      bla Nothing "abc" 'a' "bc" `shouldBe` Nothing
+      bla Nothing "abc" 'a' "bc" 0 `shouldBe` Nothing
 
     it "returns 0 when the substring matches from the start" $ do
-      bla (Just 0) "abc" 'a' "bc" `shouldBe` (Just 0)
+      bla (Just 0) "abc" 'a' "bc" 0 `shouldBe` (Just 0)
+
+    it "adds the cutoff to the result" $ do
+      bla (Just 0) "abc" 'a' "bc" 12 `shouldBe` (Just 12)
 
     it "returns 5 when the substring matches from position 5" $ do
-      bla (Just 5) "12345abc" 'a' "bc" `shouldBe` (Just 5)
+      bla (Just 5) "12345abc" 'a' "bc" 0 `shouldBe` (Just 5)
 
   describe "substringPos" $ do
     it "returns Nothing when the substring is empty" $ do
-      substringPos "abcde" "" `shouldBe` Nothing
+      substringPos "abcde" "" 0 `shouldBe` Nothing
 
     it "returns Nothing when the string to search in is empty" $ do
-      substringPos "" "abc" `shouldBe` Nothing
+      substringPos "" "abc" 0 `shouldBe` Nothing
 
     it "returns the position of the first occurrence of the first char when the search string fully matches at that position (start of string)" $ do
-      substringPos "abcde" "abc" `shouldBe` (Just 0)
+      substringPos "abcde" "abc" 0 `shouldBe` (Just 0)
+
+    it "adds the cutoff to the position" $ do
+      substringPos "abcde" "abc" 15 `shouldBe` (Just 15)
 
     it "returns the position of the first occurrence of the first char when the search string fully matches at that position (middle of string)" $ do
-      substringPos "abcdef" "cde" `shouldBe` (Just 2)
+      substringPos "abcdef" "cde" 0 `shouldBe` (Just 2)
 
     it "returns the position of the first occurrence of the first char when the search string fully matches at that position (end of string)" $ do
-      substringPos "abcdef" "ef" `shouldBe` (Just 4)
+      substringPos "abcdef" "ef" 0 `shouldBe` (Just 4)
 
     it "returns Nothing when the search string is not contained in the string" $ do
-      substringPos "abcde" "fg" `shouldBe` Nothing
+      substringPos "abcde" "fg" 0 `shouldBe` Nothing
 
     it "returns Nothing when the search string is not fully contained in the string (start)" $ do
-      substringPos "abcde" "abx" `shouldBe` Nothing
+      substringPos "abcde" "abx" 0 `shouldBe` Nothing
 
     it "returns Nothing when the search string is not fully contained in the string (middle)" $ do
-      substringPos "abcde" "bcx" `shouldBe` Nothing
+      substringPos "abcde" "bcx" 0 `shouldBe` Nothing
 
     it "returns Nothing when the search string is not fully contained in the string (end)" $ do
-      substringPos "abcde" "dex" `shouldBe` Nothing
+      substringPos "abcde" "dex" 0 `shouldBe` Nothing
+
+    it "returns the correct position when the search string is partially and fully contained in the string" $ do
+      substringPos "abcabcde" "abcd" 0 `shouldBe` (Just 3)
+
+    it "returns the correct position when the search string is partially (multiply) and fully contained in the string" $ do
+      substringPos "abcabaabcde" "abcd" 0 `shouldBe` (Just 6)
 
