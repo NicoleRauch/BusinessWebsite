c44e4e47c6e4d76cf5766efa6c2f6c72c80c51ce
Nicole Rauch <nicole.m@gmx.de>
2016-07-31 18:10:36 +0200

    Now we strip off any paragraph HTML that might be wrapped around our excerpt.

--- hakyll/stringManipulation.hs
+++ hakyll/stringManipulation.hs
@@ -1,9 +1,20 @@
 module StringManipulation where
 
-import           Data.List (elemIndex, isPrefixOf)
+import           Data.List (elemIndex, isPrefixOf, isSuffixOf)
 
 getExcerpt :: String -> String
-getExcerpt x = untilCount x $ substringPos x "\n\n" 0
+getExcerpt post = let rawExcerpt = untilCount post $ substringPos post "\n\n" 0
+               in removePrefix $ removeSuffix rawExcerpt
+
+removePrefix :: String -> String
+removePrefix string
+        | isPrefixOf "<p>" string = drop 3 string
+        | otherwise               = string
+
+removeSuffix :: String -> String
+removeSuffix string
+        | isSuffixOf "</p>" string = take (length string - 4) string
+        | otherwise                = string
 
 untilCount :: String -> Maybe Int -> String
 untilCount _ Nothing = ""
