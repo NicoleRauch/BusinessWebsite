d8da0b4077e8c9e377f446da6391c07a8b221129
Nicole Rauch <nicole.m@gmx.de>
2016-08-17 00:24:54 +0200

    Conditional copying does not work because Hakyll keeps deleting the target files, and shake does not notice that. So let's just copy the files unconditionally.

--- Build.hs
+++ Build.hs
@@ -27,26 +27,24 @@ main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
         need [".bootstrap_fonts", ".fontawesome_fonts"] 
         need [".jquery_js", ".bootstrap_js", ".jquery_easing_js", ".fullpage_js"]
 
-    ".bootstrap_fonts" %> copyFiles "node_modules/bootstrap/dist/fonts" "HTML/fonts"
-    ".fontawesome_fonts" %> copyFiles "node_modules/font-awesome/fonts" "HTML/fonts"
+    ".bootstrap_fonts" ~> copyFiles "node_modules/bootstrap/dist/fonts" "HTML/fonts"
+    ".fontawesome_fonts" ~> copyFiles "node_modules/font-awesome/fonts" "HTML/fonts"
 
-    ".fullpage_css" %> copyFile "node_modules/fullpage.js" "jquery.fullPage.css" "hakyll/css"
+    ".fullpage_css" ~> copyFile "node_modules/fullpage.js" "jquery.fullPage.css" "hakyll/css"
 
-    ".jquery_js" %> copyFile "node_modules/jquery/dist" "jquery.min.js" "HTML/js"
-    ".bootstrap_js" %> copyFile "node_modules/bootstrap/dist/js" "bootstrap.min.js" "HTML/js"
-    ".jquery_easing_js" %> copyFile "node_modules/jquery.easing" "jquery.easing.min.js" "HTML/js"
-    ".fullpage_js" %> copyFile "node_modules/fullpage.js" "jquery.fullPage.js" "HTML/js"
+    ".jquery_js" ~> copyFile "node_modules/jquery/dist" "jquery.min.js" "HTML/js"
+    ".bootstrap_js" ~> copyFile "node_modules/bootstrap/dist/js" "bootstrap.min.js" "HTML/js"
+    ".jquery_easing_js" ~> copyFile "node_modules/jquery.easing" "jquery.easing.min.js" "HTML/js"
+    ".fullpage_js" ~> copyFile "node_modules/fullpage.js" "jquery.fullPage.js" "HTML/js"
 
 
-copyFile from filename to = \out -> do
+copyFile from filename to = do
         unit $ cmd "mkdir -p" to
         copyFile' (from </> filename) (to </> filename)
-        writeFile' out ""
 
-copyFiles from to = \out -> do
+copyFiles from to = do
         unit $ cmd "mkdir -p" to
         files <- getDirectoryFiles from ["*"]
         forM_ files $ \file -> do
             copyFile' (from </> file) (to </> file)
-        writeFile' out ""
 
